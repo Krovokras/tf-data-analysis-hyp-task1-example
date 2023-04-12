7e553ff30b161b178173bea8bbfd82c9c4d14e80
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-12 20:54:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,32 @@
-from scipy import stats
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
-chat_id = 1134491798 
+chat_id = 1134491798 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int, alpha: float = 0.06) -> bool:
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+  
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
-    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
-    difference = p1 - p2
-    z_value = difference / (p_combined * (1 - p_combined) * (1/x_cnt + 1/y_cnt)) ** 0.5
-    p_value = 1 - stats.norm.cdf(z_value)
-    return p_value < alpha
+    
+   
+    se = np.sqrt(p1*(1-p1)/x_cnt + p2*(1-p2)/y_cnt)
+    
+   
+    z = (p1 - p2) / se
+    
+   
+    p_value = norm.sf(abs(z))*2
+    
+   
+    if p_value < 0.06:
+        return True
+    else:
+        return False
 x_success = 1000
 x_cnt = 3000
 y_success = 5000
 y_cnt = 6400
-print(solution(x_success, x_cnt, y_success, y_cnt))
